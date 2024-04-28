c517c59544ee899ebf6baccade221ebae724bf6f
maostageAndes <m.castror2@uniandes.edu.co>
2024-04-27 19:36:44 -0500

    clase vehiculo y estructuracion tabla

--- src/app/vehiculo/vehiculo.component.ts
+++ src/app/vehiculo/vehiculo.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Vehiculo } from './vehiculo';
 
 @Component({
   selector: 'app-vehiculo',
@@ -7,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VehiculoComponent implements OnInit {
 
+  vehiculos: Array<Vehiculo> =[];
   constructor() { }
 
   ngOnInit() {
